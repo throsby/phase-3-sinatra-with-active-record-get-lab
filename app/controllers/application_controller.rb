9a87d182be7fb56609eaf18e7bf032555320e535
Throsby Wells <throsbywells@gmail.com>
2022-05-10 12:08:48 -0400

    commit tree 289f86437d9c03d066d4c8844762f90652941168

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get "/bakeries" do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get "/bakeries/:id" do
+    bakery = Bakery.all.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get "/baked_goods/by_price" do
+    bg_prices_decr = BakedGood.all.order("price DESC")
+    bg_prices_decr.to_json
+  end
+
+  get "/baked_goods/most_expensive" do
+    most_expensive = BakedGood.all.order("price DESC").take(1)[0]
+    most_expensive.to_json
+  end
 
 end
--- spec/01_models/bakery_spec.rb
+++ spec/01_models/bakery_spec.rb
@@ -5,7 +5,7 @@ describe Bakery do
   before do
     bakery1 = Bakery.create(name: "Northside")
     bakery2 = Bakery.create(name: "Southside")
-    BakedGood.create(name: "Croissant", price: 3, bakery_id: bakery1.id)
+    BakedGood.create(name: "Croissant", price: 5, bakery_id: bakery1.id)
     BakedGood.create(name: "Bagel", price: 2, bakery_id: bakery2.id)
     BakedGood.create(name: "Banana Bread", price: 3, bakery_id: bakery2.id)
   end
